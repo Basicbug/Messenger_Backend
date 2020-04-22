e3ccbe17b97557542c0ec929471e8358bb74be73
qwebnm7788 <qwebnm7788@naver.com>
2020-04-22 23:29:21 +0900

    initial commit
    Signed-off-by: qwebnm7788 <qwebnm7788@naver.com>

---
+++ src/test/java/com/basicbug/messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicbug.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
