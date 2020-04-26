f49d8b0577ec154d8d29ba1be41abd159d1c631c
Jaewon Choi <qwebnm7788@naver.com>
2020-04-26 23:06:04 +0900

    Api Response 표준화 작업 (#8)
    모든 API 호출 결과에 대해 동일한 형태의 응답을 내려주기 위한 Response 모듈 작성

---
+++ src/main/java/com/basicbug/messenger/service/ResponseService.java
@@ -0,0 +1,64 @@
+package com.basicbug.messenger.service;
+
+import com.basicbug.messenger.model.response.CommonResponse;
+import com.basicbug.messenger.model.response.ListResponse;
+import com.basicbug.messenger.model.response.SingleResponse;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author JaewonChoi
+ */
+
+@Service
+public class ResponseService {
+
+    public enum CommonResult {
+        SUCCESS(0, "성공"),
+        FAIL(-1, "실패");
+
+        int code;
+        String message;
+
+        CommonResult(int code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+
+        public int getCode() {
+            return this.code;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+    }
+
+    public <T> SingleResponse<T> getSingleResponse(T data) {
+        SingleResponse<T> response = new SingleResponse<>();
+        response.setData(data);
+        setSuccessfulResult(response);
+
+        return response;
+    }
+
+    public <T> ListResponse<T> getListResponse(List<T> dataList) {
+        ListResponse<T> response = new ListResponse<>();
+        response.setDataList(dataList);
+        setSuccessfulResult(response);
+
+        return response;
+    }
+
+    private void setFailResult(CommonResponse response) {
+        response.setSuccess(false);
+        response.setCode(CommonResult.FAIL.getCode());
+        response.setMessage(CommonResult.FAIL.getMessage());
+    }
+
+    private void setSuccessfulResult(CommonResponse response) {
+        response.setSuccess(true);
+        response.setCode(CommonResult.SUCCESS.getCode());
+        response.setMessage(CommonResult.SUCCESS.getMessage());
+    }
+}
