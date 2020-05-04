9be09b60a92c7e7dd1e653f09d4b2b41bd831fe5
qwebnm7788 <qwebnm7788@naver.com>
2020-05-04 15:57:07 +0900

    dev 환경에서 Spring security 예외 설정
    개발 편의성을 위해 dev 환경에서 시큐리티 설정을 제거

--- src/main/java/com/basicbug/messenger/config/security/SecurityConfiguration.java
+++ src/main/java/com/basicbug/messenger/config/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.basicbug.messenger.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @RequiredArgsConstructor
 @Configuration
+@Profile("prod")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
