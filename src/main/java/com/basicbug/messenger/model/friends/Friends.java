cf96b0d81ddb07fb45e5218869c4f383e3da590d
qwebnm7788 <qwebnm7788@naver.com>
2020-05-04 15:56:20 +0900

    친구 관계 Entity 정의
    - User, Friends 테이블에 기본 더미 데이터 추가

---
+++ src/main/java/com/basicbug/messenger/model/friends/Friends.java
@@ -0,0 +1,40 @@
+package com.basicbug.messenger.model.friends;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+/**
+ * @author JaewonChoi
+ */
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "friends")
+public class Friends {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 30)
+    private String fromuid;
+
+    @Column(nullable = false, length = 30)
+    private String touid;
+
+    @CreatedDate
+    private LocalDateTime meetAt;
+}
