0b7c9b48402f711ffa9fc7283ecacc1b05f4d66b
qwebnm7788 <qwebnm7788@naver.com>
2020-05-25 23:49:41 +0900

    Add @EnableSocket annotation to config
    Signed-off-by: qwebnm7788 <qwebnm7788@naver.com>

--- src/main/java/com/basicbug/messenger/config/talk/TalkSocketConfig.java
+++ src/main/java/com/basicbug/messenger/config/talk/TalkSocketConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -14,6 +15,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  */
 
 @Configuration
+@EnableWebSocket
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class TalkSocketConfig implements WebSocketMessageBrokerConfigurer {
