7822080573936da09a1cd656eb611a273c61c761
qwebnm7788 <qwebnm7788@naver.com>
2020-05-31 23:31:30 +0900

    Stomp connect 연결 시 token verification 추가
    connection 이후에는 이미 인증되었다고 생각되기 때문에 따로 검증할 필요가 없다.

--- src/main/java/com/basicbug/messenger/config/handler/StompHandler.java
+++ src/main/java/com/basicbug/messenger/config/handler/StompHandler.java
@@ -1,9 +1,11 @@
 package com.basicbug.messenger.config.handler;
 
+import com.basicbug.messenger.config.security.JwtTokenProvider;
 import com.basicbug.messenger.model.user.User;
 import com.basicbug.messenger.repository.talk.TalkRoomRepository;
 import com.basicbug.messenger.repository.user.UserRepository;
 import com.basicbug.messenger.service.talk.TalkService;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -11,6 +13,9 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 /**
@@ -25,17 +30,31 @@ public class StompHandler implements ChannelInterceptor {
     private final TalkService talkService;
     private final TalkRoomRepository talkRoomRepository;
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
         if (StompCommand.CONNECT == accessor.getCommand()) {
-            //TODO connection needs to be authenticated. also simpUser header needs to be added.
-            log.info("connect request");
+            if (accessor.getFirstNativeHeader("Authorization") == null) {
+                log.info("Stomp connect request but not Authorization header");
+                return null;
+            }
+
+            String token = jwtTokenProvider.resolveToken(accessor.getFirstNativeHeader("Authorization"));
+            log.info("Stomp connect request with " + token);
+
+            if (!jwtTokenProvider.validateToken(token)) {
+                log.info("Stomp connect request but invalid token");
+                return null;
+            }
+
+            accessor.setUser(jwtTokenProvider.getAuthentication(token));
+            log.info("Stomp connect success " + accessor.getUser().getName());
+
         } else if (StompCommand.SUBSCRIBE == accessor.getCommand()) {
             String roomId = talkService.getRoomId(accessor.getDestination());
-            log.info("stomp subscribe to roomId " + roomId);
 
             if (talkRoomRepository.findTalkRoomById(roomId) == null) {
                 log.error("stomp subscribe to room that does not exists " + roomId);
