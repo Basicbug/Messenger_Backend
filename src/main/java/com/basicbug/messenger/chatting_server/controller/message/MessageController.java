5ef5903974651584019e8a612b1e8a920f9bfedb
qwebnm7788 <qwebnm7788@naver.com>
2020-06-10 20:54:03 +0900

    Rabbitmq listener 위치 수정

--- src/main/java/com/basicbug/messenger/chatting_server/controller/message/MessageController.java
+++ src/main/java/com/basicbug/messenger/chatting_server/controller/message/MessageController.java
@@ -1,12 +1,16 @@
 package com.basicbug.messenger.chatting_server.controller.message;
 
+import com.basicbug.messenger.chatting_server.config.rabbitmq.RabbitMQDevConfig;
 import com.basicbug.messenger.chatting_server.controller.rabbitmq.RabbitMQUtil;
 import com.basicbug.messenger.api_server.model.message.TalkMessage;
 import com.basicbug.messenger.api_server.service.talk.TalkRoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -21,10 +25,17 @@ public class MessageController {
     private final RabbitMQUtil rabbitMQUtil;
     private final RabbitTemplate rabbitTemplate;
     private final TalkRoomService talkRoomService;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/talk/message")
     public void sendMessage(TalkMessage message) {
         log.debug("sendMessage in /talk/message " + message.getRoomId());
         rabbitMQUtil.sendMessage(message);
+    }
+
+    @RabbitListener(queues = RabbitMQDevConfig.QUEUE_NAME)
+    public void onMessage(TalkMessage message) {
+        log.info("onMessage " + message.toString());
+        simpMessagingTemplate.convertAndSend("/sub/talk/room/" + message.getRoomId(), message);
     }
 }
