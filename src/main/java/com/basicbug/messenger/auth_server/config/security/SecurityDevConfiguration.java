1d40e5209d641f211f29ed8f2aa6f4e541d0491e
qwebnm7788 <qwebnm7788@naver.com>
2020-06-16 23:55:55 +0900

    OAuth 서버 설정 제거
    - JWT 토큰과 Filter 를 이용한 인증으로 수정
    - SecurityConfiguration 을 Secured/NonSecured API 로 구분하여 설정을 새롭게 적용하여
      Secured 인 경우에만 JWT filter 인증 로직을 타도록 수정

--- src/main/java/com/basicbug/messenger/auth_server/config/security/SecurityDevConfiguration.java
+++ src/main/java/com/basicbug/messenger/auth_server/config/security/SecurityDevConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author JaewonChoi
@@ -20,46 +23,61 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Profile("dev")
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityDevConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityDevConfiguration {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final CustomAuthenticationProvider customAuthenticationProvider;
+    @Configuration
+    @Order(1)
+    public static class NonSecuredApiConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authenticationProvider(customAuthenticationProvider)
-            .httpBasic().disable()
-            .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .headers()
-            .frameOptions().disable()
-            .and()
-            .authorizeRequests()
-            .antMatchers("/stomp/**").hasRole("USER")
-//            .antMatchers( "/*/signin", "/*/signup", "/social/**", "/v1/**", "/stomp/**").permitAll()
-            .antMatchers("/v1/test/**").authenticated();
-//            .anyRequest()
-//            .hasRole("USER")
-//            .and()
-//            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .requestMatchers()
+                .antMatchers("/stomp/**", "/v1/test/**", "/v1/social/**")
+                .and()
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        }
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web
+                .ignoring()
+                .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**", "/h2-console/**");
+        }
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-            .ignoring()
-            .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**", "/h2-console/**");
+    @Configuration
+    @Order(2)
+    @RequiredArgsConstructor
+    public static class SecuredApiConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        private final JwtTokenProvider jwtTokenProvider;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .requestMatchers()
+                .antMatchers("/v1/**")
+                .and()
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        }
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(customAuthenticationProvider);
-//            .inMemoryAuthentication()
-//            .withUser("qwebnm7788")
-//            .password("password")
-//            .roles("USER");
-    }
 
 }
