1437c4abc529b2cdd5dc6949616040b27ee7f6c6
qwebnm7788 <qwebnm7788@naver.com>
2020-06-20 22:31:26 +0900

    Test Controller 에서도 실제 유저 정보를 등록하도록 수정
    - 유저 정보를 등록하지 않은 채로 jwt token 을 제공하면 인증 과정에서 UserNotFoundException 이 발생하며
      실패하게 된다.

--- src/main/java/com/basicbug/messenger/api_server/controller/test/TestController.java
+++ src/main/java/com/basicbug/messenger/api_server/controller/test/TestController.java
@@ -2,12 +2,16 @@ package com.basicbug.messenger.api_server.controller.test;
 
 import com.basicbug.messenger.api_server.dto.social.JwtTokenResponse;
 import com.basicbug.messenger.api_server.model.response.SingleResponse;
+import com.basicbug.messenger.api_server.model.user.User;
+import com.basicbug.messenger.api_server.repository.user.UserRepository;
 import com.basicbug.messenger.api_server.service.ResponseService;
 import com.basicbug.messenger.auth_server.config.security.JwtTokenProvider;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +32,7 @@ public class TestController {
 
     private final ResponseService responseService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
 
     @ApiOperation(value = "테스트용 jwt 토큰 생성", notes = "임시로 사용할 jwt 토큰 생성")
     @PostMapping("/create/token")
@@ -35,6 +40,22 @@ public class TestController {
         @ApiParam(value = "사용자 uid", required = true) @RequestParam String uid
     ) {
         log.info("createJwtTokenForTestUser " + uid);
-        return responseService.getSingleResponse(new JwtTokenResponse(jwtTokenProvider.createToken(uid, Arrays.asList("ROLE_USER"))));
+
+        String name = "TEST-" + UUID.randomUUID();
+
+        User user = userRepository.findByUidAndProvider(uid, "").orElse(null);
+        if (user == null) {
+            userRepository.save(User.builder()
+                .uid(uid)
+                .provider("")
+                .name(name)
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build());
+
+            user = userRepository.findByUidAndProvider(uid, "").orElse(null);
+        }
+
+        return responseService
+            .getSingleResponse(new JwtTokenResponse(jwtTokenProvider.createToken(String.valueOf(user.getUid()), user.getRoles())));
     }
 }
