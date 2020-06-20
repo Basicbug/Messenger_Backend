f4cb93202fc285748a0c544aaab2945c71eba45b
qwebnm7788 <qwebnm7788@naver.com>
2020-06-20 22:02:29 +0900

    TalkRoomDetailResponse 의 불필요한 정보 제거

--- src/main/java/com/basicbug/messenger/api_server/dto/talk/response/TalkRoomDetailResponseDto.java
+++ src/main/java/com/basicbug/messenger/api_server/dto/talk/response/TalkRoomDetailResponseDto.java
@@ -3,6 +3,7 @@ package com.basicbug.messenger.api_server.dto.talk.response;
 import com.basicbug.messenger.api_server.model.message.TalkMessage;
 import com.basicbug.messenger.api_server.model.message.TalkRoom;
 import com.basicbug.messenger.api_server.model.user.User;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,14 +24,17 @@ public class TalkRoomDetailResponseDto {
 
     private String name;
 
-    private List<User> participants;
+    private List<String> participants;
 
     private TalkMessage lastMessage;
 
     public TalkRoomDetailResponseDto(TalkRoom talkRoom) {
         this.roomId = talkRoom.getRoomId();
         this.name = talkRoom.getName();
-        this.participants = talkRoom.getParticipants();
         this.lastMessage = talkRoom.getLastMessage();
+        this.participants = new ArrayList<>();
+        for (User user : talkRoom.getParticipants()) {
+            participants.add(user.getUid());
+        }
     }
 }
